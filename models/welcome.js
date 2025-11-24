69a934da7e9d80990b7b33369ef4054c4e521c15
Nexus833 <nexuscompany.net@gmail.com>
2025-11-24 12:01:48 +0330

    Update welcome.js

--- models/welcome.js
+++ models/welcome.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const welcomeSchema = new mongoose.Schema({
   serverId: { type: String, required: true, unique: true },
-  enabled: { type: Boolean, default: false },
   description: { type: String, default: 'Welcome {member} to {server}' },
   channelId: { type: String, default: null },
 });
