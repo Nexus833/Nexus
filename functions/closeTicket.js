2124e7d50047c6f327bbf8f2503e53e68d463b3b
Nexus833 <nexuscompany.net@gmail.com>
2025-11-24 12:14:51 +0330

    Update closeTicket.js

--- functions/closeTicket.js
+++ functions/closeTicket.js
@@ -44,7 +44,7 @@ async function closeTicket(channel, closer, reason = 'No reason provided') {
         { name: 'Closed By', value: closer.tag, inline: true },
         { name: 'Reason', value: reason, inline: true },
       ])
-      .setTimestamp();
+      .Nexus Ticket System;
 
     if (settings?.logChannelId) {
       const logChannel = channel.guild.channels.cache.get(
